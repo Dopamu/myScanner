c702e7fd6f2d2cd7cef8e75e406fc7614b7044a8
Dopamu <55927934+Dopamu@users.noreply.github.com>
2020-02-26 07:25:13 +0000

    Update Homepage.java

--- main/java/com/example/myscanner/Homepage.java
+++ main/java/com/example/myscanner/Homepage.java
@@ -1,65 +1,158 @@
 package com.example.myscanner;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-public class Homepage extends AppCompatActivity  implements View.OnClickListener {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
-    Button bScan;
-    Button bViewAtt;
-    private final int permission_code = 1;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.security.auth.callback.Callback;
+
+
+public class login extends AppCompatActivity implements View.OnClickListener {
+
+
+    Callback apiCallback = null;
+    Button bLogin;
+    EditText etUsername, etPassword, etModule;
+    TextView tvRegisterLink;
+    private Session session;
+    private DbHelper db;
+
+    public RequestQueue requestQueue;
+
 
     @Override
-    protected void onCreate (Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.activity_login);
 
-        bScan = findViewById(R.id.bScan);
-        bViewAtt = findViewById(R.id.bView_Att);
+        bLogin = findViewById(R.id.bLogin);
+        etPassword = findViewById(R.id.etPassword);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etModule = findViewById(R.id.etModule);
+        tvRegisterLink = findViewById(R.id.tvRegisterLink);
 
+        db = new DbHelper(this);
+        session = new Session(this);
+        bLogin.setOnClickListener(this);
+        tvRegisterLink.setOnClickListener(this);
 
-        bScan.setOnClickListener(this);
-        bViewAtt.setOnClickListener(this);
-
+        if(session.loggedin()){
+            startActivity(new Intent(login.this,Homepage.class));
+            finish();
+        }
+        //userlocalStore = new localstore(this);
     }
 
-    // users must grant permission to open camera
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == permission_code) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Permission Granted", Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(this, "Permission Denied", Toast.LENGTH_LONG).show();
-            }
-        }
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, permission_code);
-            return;
-        }
-    }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
-    public void onClick (View v) {
+    public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.bScan:
-                startActivity(new Intent(this, ScanCode.class));
-                break;
+            case R.id.bLogin:
 
-            case R.id.bView_Att:
-                startActivity(new Intent(this, ViewAttendance.class));
+                //// for testing and debuging //
+                String URL = "http://34.241.26.73/log";
+                requestQueue = Volley.newRequestQueue(this);
+                StringRequest jsonObjRequest = new StringRequest(Request.Method.POST,
+                        URL,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                try {
+                    JSONObject objres = new JSONObject(response);
+                    Toast.makeText(getApplicationContext(), objres.toString(), Toast.LENGTH_LONG).show();
+
+
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(), "Server Error", Toast.LENGTH_LONG).show();
+
+                }
+
+                            }
+                        }, new Response.ErrorListener() {
+
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+
+                    @Override
+                    public String getBodyContentType() {
+                        return "application/json; charset=UTF-8";
+                    }
+
+
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        String username = etUsername.getText().toString().trim();
+                        String password = etPassword.getText().toString().trim();
+
+                        Map<String, String> postParam = new HashMap<String, String>();
+
+                        postParam.put("username", username);
+                        postParam.put("password", password);
+
+
+                        return postParam;
+                    }
+
+                };
+                requestQueue.add(jsonObjRequest);
+                log();
+
                 break;
+            case R.id.tvRegisterLink:  // links the register page to the login page
+                startActivity(new Intent(this, Register.class));
+                break;
+            default:
+
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void log() {
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+        String module = etModule.getText().toString();
+
+        if (db.getUser(username, password, module)) {
+            session.setLoggedin(true);
+            Toast.makeText(getApplicationContext(), "Welcome back "+ username,Toast.LENGTH_SHORT).show();
+            SharedPreferences sharedPreferences = getSharedPreferences("Mypref", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("Username", username);
+            editor.putString("Module code", module);
+            editor.apply();
+
+            startActivity(new Intent(login.this, Homepage.class));
+            finish();
+        }else{
+            Toast.makeText(getApplicationContext(), "Wrong username/password",Toast.LENGTH_SHORT).show();
         }
     }
 }
